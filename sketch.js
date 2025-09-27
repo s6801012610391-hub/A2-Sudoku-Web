844da7c5cf40d53d851f0907d1c9624b71d7aa50
pongsapak <s6801012610391@email.kmutnb.ac.th>
2025-09-27 21:36:44 +0700

    add function draw_table

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+function draw_table() {
+    let i = 0;
+    while (i<8) {
+        strokeWeight(1);
+        if (i==2 || i==5) {
+            strokeWeight(2);
+        }
+        line(67+67*i,0,67+67*i,height);
+        i += 1;
+    }
+
+    let j = 0;
+    while (j<8) {
+        strokeWeight(1);
+        if (j==2 || j ==5) {
+            strokeWeight(2);
+        }
+        line(0,67+67*j,width,67+67*j);
+        j += 1;
+    }
+}
+
 function setup() {
     createCanvas(600, 600);
 }
 
 function draw() {
-    background(255);
+    draw_table();
 }
